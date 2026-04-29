078166f056df6c986afa4f6459908e3a57c9e552
Jose Armando Sandoval Santana <sandovalsantanajosearmando@gmail.com>
2026-04-28 22:31:25 -0600

    cambios 4

--- src/main/java/com/api/NubeSandovalAPI/NubeSandovalApiApplication.java
+++ src/main/java/com/api/NubeSandovalAPI/NubeSandovalApiApplication.java
@@ -3,7 +3,9 @@ package com.api.NubeSandovalAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration.class
+})
 public class NubeSandovalApiApplication {
 
 	public static void main(String[] args) {
